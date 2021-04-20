01d2fc6c502ee78062cb7b68620f6e31a2f9d8ac
Jan Jaroszczak <jan.jaroszczak@gmail.com>
2021-04-20 23:15:27 +0200

    ADDED: footer content.

--- src/components/organisms/navigations/Footer.js
+++ src/components/organisms/navigations/Footer.js
@@ -1,9 +1,25 @@
 import React from 'react';
 
-import { StyledFooter } from './styles/StyledFooter';
+import { routes } from '../../../routes';
+
+import {
+  StyledFooter,
+  StyledUl,
+  StyledLi,
+  StyledLink,
+} from './styles/StyledFooter';
 
 const Footer = () => {
-  return <StyledFooter></StyledFooter>;
+  return (
+    <StyledFooter>
+      <StyledUl>
+        <StyledLi>{`Copyright \u00A9 ${new Date().getFullYear()} Jan Jaroszczak`}</StyledLi>
+        <StyledLi>
+          <StyledLink to={routes.products}>Terms and Conditions</StyledLink>
+        </StyledLi>
+      </StyledUl>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
--- src/components/organisms/navigations/styles/StyledFooter.js
+++ src/components/organisms/navigations/styles/StyledFooter.js
@@ -1,7 +1,39 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const StyledFooter = styled.footer`
   width: 100%;
   height: 70px;
+  font-size: ${({ theme }) => theme.fontSizes.xxs};
+  color: ${({ theme }) => theme.colors.mainWhite};
   background-color: ${({ theme }) => theme.colors.lightDark};
+
+  @media (max-width: 400px) {
+    font-size: ${({ theme }) => theme.fontSizes.ss};
+  }
+`;
+
+export const StyledUl = styled.ul`
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* margin: 0 auto; */
+
+  @media (max-width: 400px) {
+    flex-direction: column;
+  }
+`;
+
+export const StyledLi = styled.li`
+  margin: 0 20px;
+
+  @media (max-width: 400px) {
+    margin: 5px 0;
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  color: ${({ theme }) => theme.colors.mainWhite};
+  text-decoration: none;
 `;
