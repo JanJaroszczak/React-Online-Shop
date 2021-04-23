59d2f8796b76de52d5b7c3d1e53709da7e8b3921
Jan Jaroszczak <jan.jaroszczak@gmail.com>
2021-04-23 22:13:24 +0200

    CODE CLEANUP

--- src/components/molecules/CartSummary.js
+++ src/components/molecules/CartSummary.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 import { cartSummaryVariants } from '../../helpers/atomsTypesAndVariants';
 import { routes } from '../../routes';
