2c590819b0de8a892a5c02793c78101f4893dd0c
Jan Jaroszczak <jan.jaroszczak@gmail.com>
2021-04-25 20:24:02 +0200

    IMPROVED: HomeCategories refactoring.

--- src/components/molecules/HomeCategories.js
+++ src/components/molecules/HomeCategories.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import ProductCategory from '../atoms/ProductCategory';
 
+import { brandNames } from './helpers/brandNamesForHomeCategories';
+
 import { StyledCategoriesWrapper } from './styles/StyledHomeCategories';
 
 import category1 from '../../assets/images/category1.png';
@@ -9,11 +11,13 @@ import category2 from '../../assets/images/category2.png';
 import category3 from '../../assets/images/category3.jpg';
 
 const HomeCategories = () => {
+  const { brand1, brand2, brand3 } = brandNames;
+
   return (
     <StyledCategoriesWrapper>
-      <ProductCategory img={category1} value={'nike'} />
-      <ProductCategory img={category2} value={'adidas'} />
-      <ProductCategory img={category3} value={'puma'} />
+      <ProductCategory img={category1} value={brand1} />
+      <ProductCategory img={category2} value={brand2} />
+      <ProductCategory img={category3} value={brand3} />
     </StyledCategoriesWrapper>
   );
 };
---
+++ src/components/molecules/helpers/brandNamesForHomeCategories.js
@@ -0,0 +1,5 @@
+export const brandNames = {
+  brand1: 'nike',
+  brand2: 'adidas',
+  brand3: 'puma',
+};
